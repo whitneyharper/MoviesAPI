d3bdd180445800da8c6e4daba3573072676187cb
Whit <whitneyharper1@aol.com>
2022-09-28 15:58:23 -0400

    Created Data folder with a file defining Movie Context. Added db connection string.

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
