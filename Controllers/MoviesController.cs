d070c0ecd33099236505b77561c7e9a73b552ebb
Whit <whitneyharper1@aol.com>
2022-09-28 21:08:56 -0400

    Create Movies controller and created GetMovies method.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieDbContext _db;
+
+        public MoviesController(MovieDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if (_db.Movies == null)
+            {
+                return NotFound();
+            }
+            return await _db.Movies.ToListAsync();
+        }
+    }
+}
